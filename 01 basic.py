d8004b23c084d1bd4c5be505d0d2da5f4307ad08
rapnyt <rapnyt@gmail.com>
2020-04-29 15:14:49 +0200

    Every file created and edited to meet the desired category.

--- 01 basic.py
+++ 01 basic.py
@@ -1,1 +1,65 @@
+'''
+Here are all the required base basics
+'''
+
+print("Hello world")
+print("First string" + ", " + "second string")  # print with strings
+my_age = "32"
+
+my_desired_age = int(my_age) - 10  # unit conversion
+
+my_boolean = True
+print(my_boolean)  # booleans
+print(2 == 3)
+
+if my_desired_age < int(my_age):
+    print("You sick bastard")
+else:  # conditionals
+    print("You'd wish")
+
+if "fuck" == "cunt":
+    print("luck")
+elif "fuck" == "slack":
+    print("damn")
+elif True:
+    print("yay")
+
+my_age = int(my_age)
+
+while my_desired_age < my_age:  # while loop
+    print(my_desired_age)
+    my_desired_age += 1
+
+i = 0
+
+while True:  # infinite while loop with break point and ommittments
+    i += 1
+    if i % 2 == 0:
+        continue
+    print(i)
+    if i > 51:
+        break
+
+my_age == my_desired_age or my_age == my_desired_age and my_boolean != my_desired_age  # operators
+
+words = ["hello", "fuck","no",[1,2,3]] # lists
+words[0] = "lalaa"
+print(words[0],words[3][1])
+print(words[3]*5)
+print("fuck" in words)
+print(not "nosdf" in words)
+empty_list = []
+empty_list.append(4)
+print(len(empty_list))
+empty_list.insert(0,"to be")
+print(empty_list)
+print(empty_list.index("to be"))
+
+numbers = list(range(10,20,3)) # range
+print(numbers)
+
+for skfhdklfh in words: # for loops
+    print(str(skfhdklfh) + "!")
+for j in range(5):
+    print("fuck ")
 
--- 02 functions_and_modules.py
+++ 02 functions_and_modules.py
@@ -1,1 +1,15 @@
+def my_dick(k): # functions
+    print(k)
+    return k
+    print("This won't be printed")
 
+z = my_dick
+print(z(50))
+
+import random as ran
+from math import pi as pi_number
+
+value = ran.randint(1,6)
+pi = pi_number
+
+print(value, pi_number)
