bab8fdaebc4e4d9d3efb7070fcb7fb169b33648b
rapnyt <rapnyt@gmail.com>
2020-04-29 12:01:20 +0200

    exceptions and files file adjusted

--- exceptions_and_files.py
+++ exceptions_and_files.py
@@ -11,10 +11,34 @@ ValueError: a function is called on a value of the correct type, but with an ina
 '''
 
 try:
-    print(sfkjhsd)
+    print("sfkjhsd")
 except NameError:
     print("\nThere is no such a variable called ...")
-except (ValueError,TypeError):
-    print("kakaka")
+except (ValueError, TypeError):
+    print("This will run in case of value or type error")
 except:
-    print("jfdsjkfhds")
+    print("This will run if any error occurs")
+finally:
+    print("This code will run no matter what even if there is uncaught exception")
+
+if False:
+    raise ValueError
+    assert 2 == 4  # This is sanity check
+
+temp = 16
+assert (temp > 0), "Colder than absolute zero"
+
+myfile = open("filename.txt")
+
+'''
+You can specify the mode used to open a file by applying a second argument to the open function.
+Sending "r" means open in read mode, which is the default.
+Sending "w" means write mode, for rewriting the contents of a file.
+Sending "a" means append mode, for adding new content to the end of the file.
+
+Adding "b" to a mode opens it in binary mode, which is used for non-text files (such as image and sound files).
+'''
+
+myfile = open("filename.txt","w")
+print("I like learning coding in python")
+myfile.close()
