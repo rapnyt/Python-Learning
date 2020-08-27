754fd5c94387109052b268f3d9d50cf2f19faec7
rapnyt <64311635+rapnyt@users.noreply.github.com>
2020-08-27 09:50:09 +0200

    Create 06 classes.py

---
+++ 06 classes.py
@@ -0,0 +1,16 @@
+class Animal:                           #superclass
+    def __init__(self,colour,legs):
+        self.colour = colour
+        self.legs = legs
+
+class Cat(Animal):                      #subclass
+    def purr(self):
+        print("purrrrrrrrrrrrrr")
+        return 0
+
+felix = Cat("ginger", 4)
+
+print(felix.colour)
+print(felix.legs)
+felix.purr()
+print(felix.purr())
