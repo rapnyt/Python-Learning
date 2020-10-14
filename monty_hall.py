ff67ffaf9c5c2505d384aea1ad3ac7e549b6bcfa
rapnyt <rapnyt@gmail.com>
2020-10-14 16:48:54 +0200

    !!!!!

--- monty_hall.py
+++ monty_hall.py
@@ -15,15 +15,21 @@ for i in range(1,2):
 
     picked_gate = random.randint(1,3)
     random_empty_gate_open = random.randint(1,3)
-    while x[random_empty_gate_open]:
+    while x[random_empty_gate_open] == True:
         random_empty_gate_open = random.randint(1, 3)
 
+    for i in x:
+        if x[i] == False and i == random_empty_gate_open:
+            x[i] = None
 
     print("picked gate ", picked_gate)
     print("random empty gate ", random_empty_gate_open)
 
     if x[picked_gate]:
         win_or_loose_count += 1
+
+    for i in x:
+        print(x[i])
 
 print("count ", win_or_loose_count)
 success_rate = win_or_loose_count/1 * 100
