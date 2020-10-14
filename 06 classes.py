9d14b0292bca5860aa42f28f80df93cce92adb89
rapnyt <rapnyt@gmail.com>
2020-10-14 16:42:43 +0200

    !!!!!

--- 06 classes.py
+++ 06 classes.py
@@ -3,10 +3,12 @@ class Animal:                           #superclass
         self.colour = colour
         self.legs = legs
 
+
 class Cat(Animal):                      #subclass
     def purr(self):
         print("purrrrrrrrrrrrrr")
         return 0
+
 
 felix = Cat("ginger", 4)
 
@@ -14,5 +16,4 @@ print(felix.colour)
 print(felix.legs)
 felix.purr()
 print(felix.purr())
-print("lsdhfjdkslhfsd")
-#dslidksfjsdlkdsflkjdfgdfdfgfdfdg
+
---
+++ monty_hall.py
@@ -0,0 +1,30 @@
+import random
+win_or_loose_count = 0
+
+for i in range(1,2):
+
+    x = {}
+
+    for i in range(1,4):
+        x[i] = False
+
+    x[random.randint(1,3)] = True
+
+    for i in x:
+        print(x[i])
+
+    picked_gate = random.randint(1,3)
+    random_empty_gate_open = random.randint(1,3)
+    while x[random_empty_gate_open]:
+        random_empty_gate_open = random.randint(1, 3)
+
+
+    print("picked gate ", picked_gate)
+    print("random empty gate ", random_empty_gate_open)
+
+    if x[picked_gate]:
+        win_or_loose_count += 1
+
+print("count ", win_or_loose_count)
+success_rate = win_or_loose_count/1 * 100
+print("success rate ", success_rate, "%")
