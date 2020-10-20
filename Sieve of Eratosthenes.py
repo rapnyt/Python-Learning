8fff79e86258841d966d68b6e163cd8cc3576c2f
rapnyt <rapnyt@gmail.com>
2020-10-20 22:21:01 +0200

    !!!!!

--- Sieve of Eratosthenes.py
+++ Sieve of Eratosthenes.py
@@ -1,19 +1,17 @@
 import math
-desired_range = 10
+desired_range = 100
 x = list()
-y = list()
+
 for i in range(2, desired_range + 1):
     x.append(i)
-    y.append(i)
 
 for i in x:
     if i < math.sqrt(desired_range):
-        for j in y:
-            if i * j <= desired_range:
-                try:
-                    x.remove(i*j)
-                except(ValueError):
-                    None
+        for j in range(2, math.ceil(desired_range/i + 1)):
+            try:
+                x.remove(i*j)
+            except(ValueError):
+                None
 
 for i in x:
     print(i)
