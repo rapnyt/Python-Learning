a24250c7d73009272920da1ade03d3c2963c7493
rapnyt <rapnyt@gmail.com>
2020-11-02 23:46:07 +0100

    working on snake

--- snake_obj.py
+++ snake_obj.py
@@ -8,7 +8,7 @@ class Game(object):
         # Config
         self.fps = 5
         self.resolution = (1280, 720)
-        self.box = pygame.Rect(10, 10, 20, 20)
+        self.box = pygame.Rect(100, 100, 20, 20)
 
         # Init
         pygame.init()
@@ -46,17 +46,11 @@ class Game(object):
             self.box.x += 20
         if keys[pygame.K_KP8]:
             self.box.y -= 20
-        if self.box.x >= 1280:
-            self.box.x = 0
-        if self.box.y >= 720:
-            self.box.y = 0
-        if self.box.x < 0:
-            self.box.x = 1280
-        if self.box.y < 0:
-            self.box.y = 720
+
 
     def draw(self):
         pygame.draw.rect(self.display, (245, 125, 20), self.box)
+        pygame.draw.lines(self.display, (255, 255, 255), True, [(100, 100), (600, 100), (600, 600), (100, 600)])
 
 
 if __name__ == "__main__":
